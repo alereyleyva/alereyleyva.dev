a2cf97aae4c7ef123f0e1d17fd8c092e1ce6b0bd
AleReyLeyva <65025739+AleReyLeyva@users.noreply.github.com>
2021-08-17 05:01:37 +0200

    Move Head tag to _app.js component

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,19 @@
 import "../styles/globals.css";
 import globalStyles from "../styles/Global.module.css";
+import Head from "next/head";
 import Header from "../components/Header";
 
 function MyApp({ Component, pageProps }) {
   return (
     <div className={globalStyles.container}>
+      <Head>
+        <title>Ale Rey Leyva | Personal Portfolio</title>
+        <meta
+          name="description"
+          content="Alejandro Rey Leyva - Personal Portfolio"
+        />
+        <link rel="icon" href="/favicon.svg" />
+      </Head>
       <Header />
       <Component {...pageProps} />
     </div>
--- pages/index.js
+++ pages/index.js
@@ -5,15 +5,6 @@ import Header from "../components/Header";
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>Ale Rey Leyva | Personal Portfolio</title>
-        <meta
-          name="description"
-          content="Alejandro Rey Leyva - Personal Portfolio"
-        />
-        <link rel="icon" href="/favicon.svg" />
-      </Head>
-
       <main className={globalStyles.main}>
         <h1>Alejandro Rey Leyva</h1>
         <h2>Junior Full-Stack Developer</h2>
