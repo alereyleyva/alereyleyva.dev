1fd4aa6bfbc66a17c53f49b6c59a15f417fcdc2f
AleReyLeyva <65025739+AleReyLeyva@users.noreply.github.com>
2021-08-18 14:44:12 +0200

    Adapt index.js for tailwind css

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,10 @@
-import Head from "next/head";
-import globalStyles from "../styles/Global.module.css";
-import Header from "../components/Header";
+import RRSS from "../components/RRSS";
 
 export default function Home() {
   return (
-    <>
-      <main className={globalStyles.main}>
-        <h1>Alejandro Rey Leyva</h1>
-        <h2>Junior Full-Stack Developer</h2>
-      </main>
-    </>
+    <div className="h-96">
+      <h1>Alejandro Rey Leyva</h1>
+      <h2>Junior Full-Stack Developer</h2>
+    </div>
   );
 }
