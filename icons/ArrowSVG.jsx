bbbe79b9e32a9e48ffc4dd8314baa0219cbb6276
AleReyLeyva <65025739+AleReyLeyva@users.noreply.github.com>
2021-08-19 17:26:08 +0200

    SVG scale error

--- icons/ArrowSVG.jsx
+++ icons/ArrowSVG.jsx
@@ -7,7 +7,7 @@ export default function ArrowSVG() {
       width="24"
       height="24"
       viewBox="0 0 24 24"
-      className="fill-current text-black dark:text-gray-50"
+      className="svg-icon"
     >
       <path d="M0 7.33l2.829-2.83 9.175 9.339 9.167-9.339 2.829 2.83-11.996 12.17z" />
     </svg>
