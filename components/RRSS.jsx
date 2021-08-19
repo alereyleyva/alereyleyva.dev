233266de873e6990e0322067862ad2e4bf78e7c9
AleReyLeyva <65025739+AleReyLeyva@users.noreply.github.com>
2021-08-19 16:16:49 +0200

    Create and style necessary SVG icons

--- components/RRSS.jsx
+++ components/RRSS.jsx
@@ -1,24 +1,15 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
+import GitHubSVG from '../icons/GitHubSVG';
 import SocialLogo from './SocialLogo';
-import useThemeStatus from '../theme/useThemeStatus';
 
 export default function RRSS() {
-  const { dark } = useThemeStatus();
-  const [gitHubSRC, setGitHubSRC] = useState('githubLight.svg');
-
-  useEffect(() => {
-    setGitHubSRC(dark ? 'githubDark.svg' : 'githubLight.svg');
-  }, [dark]);
-
   return (
     <div className="flex">
+      <SocialLogo alt="GitHub" href="https://github.com/AleReyLeyva">
+        <GitHubSVG />
+      </SocialLogo>
       <SocialLogo
-        src={`/logos/${gitHubSRC}`}
-        alt="GitHub"
-        href="https://github.com/AleReyLeyva"
-      />
-      <SocialLogo
-        src="/logos/linkedin.svg"
+        src="/linkedin.svg"
         alt="Linkedin"
         href="https://www.linkedin.com/in/alejandro-rey-leyva-691a43194/"
       />
--- components/SocialLogo.jsx
+++ components/SocialLogo.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import Image from 'next/image';
 
-export default function SocialLogo({ src, href, alt }) {
+export default function SocialLogo({ src, href, alt, children }) {
   return (
     <>
       <a href={href} className="ml-4 relative w-12 h-12 sm:w-10 sm:h-10">
-        <Image src={src} layout="fill" alt={`${alt} logo`} />
+        {!children ? (
+          <Image src={src} layout="fill" alt={`${alt} logo`} />
+        ) : (
+          children
+        )}
       </a>
     </>
   );
