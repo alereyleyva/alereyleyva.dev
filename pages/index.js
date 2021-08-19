96766fa8f8e5cd80341930ebd9260cfcf30cbd1b
AleReyLeyva <65025739+AleReyLeyva@users.noreply.github.com>
2021-08-19 16:17:24 +0200

    Add personal photo to index page

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import Image from 'next/image';
 
 export default function Home() {
   return (
-    <div className="h-96">
-      <h1>Alejandro Rey Leyva</h1>
-      <h2>Junior Full-Stack Developer</h2>
-    </div>
+    <>
+      <div className="flex lg:flex-col items-center justify-evenly lg:justify-between">
+        <div className="w-2/5 lg:w-full lg:mb-16">
+          <h1 className="sm:text-5xl">Alejandro Rey Leyva</h1>
+          <h2>Junior Full-Stack Developer</h2>
+        </div>
+        <div className="relative w-96 h-96 sm:w-72 sm:h-72 md:h-80 md:w-80">
+          <Image
+            priority
+            src="/alereyleyva.jpg"
+            alt="alereyleyva photo"
+            layout="fill"
+            className="rounded-full"
+          />
+        </div>
+      </div>
+    </>
   );
 }
