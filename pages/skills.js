88996dcb5d6c864d9536b908fb5883de1a65b26b
AleReyLeyva <65025739+AleReyLeyva@users.noreply.github.com>
2021-08-22 11:27:43 +0200

    Skills page initial set up

--- pages/skills.js
+++ pages/skills.js
@@ -1,5 +1,16 @@
 import React from 'react';
+import skills from '../data/skills';
+import SkillCard from '../components/SkillCard';
 
 export default function Skills() {
-  return <h1>Skills</h1>;
+  return (
+    <div>
+      <h1>Skills</h1>
+      <div className="skills-grid md:pb-9">
+        {skills.map((skill) => (
+          <SkillCard {...skill} />
+        ))}
+      </div>
+    </div>
+  );
 }
