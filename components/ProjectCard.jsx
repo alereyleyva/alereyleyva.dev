2b70beede7bfdaf3705d6ba5ae1dc4f067da5e0c
AleReyLeyva <65025739+AleReyLeyva@users.noreply.github.com>
2021-08-23 15:08:05 +0200

    Projects view - first approach

---
+++ components/ProjectCard.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import Button from './Button';
+import CodeSVG from '../SVG/CodeSVG';
+
+export default function ProjectCard({ name, web, code, techs }) {
+  const styles = `
+    h-96 lg:h-80 sm:w-full  border-2 border-gray-100
+    flex flex-col items-center justify-around
+    p-6 rounded-lg 
+    text-gray-800 dark:text-white font-medium
+  `;
+
+  return (
+    <div className={styles}>
+      <h2 className="text-3xl lg:text-2xl font-semibold">{name}</h2>
+      <h3 className="font-mono font-normal sm:text-lg text-center mb-2">
+        {techs}
+      </h3>
+      <Button href={code}>
+        <CodeSVG />
+        Code
+      </Button>
+      <Button href={web}>🔗 Web</Button>
+    </div>
+  );
+}
---
+++ data/projects.js
@@ -0,0 +1,22 @@
+const projects = [
+  {
+    name: 'This portfolio 💻',
+    techs: 'React Next TailwindCSS',
+    web: 'https://alereyleyva.dev',
+    code: 'https://github.com/alereyleyva/alereyleyva.dev',
+  },
+  {
+    name: 'Interior Consultant 🏠',
+    techs: 'HTML CSS JS',
+    web: 'https://interior-consultant-app.vercel.app/',
+    code: 'https://github.com/AleReyLeyva/Interior-Consultant',
+  },
+  {
+    name: 'Weather App 🌞',
+    web: 'https://dev-weather-app.vercel.app/',
+    code: 'https://github.com/alereyleyva/weather-app',
+    techs: 'React Next TailwindCSS',
+  },
+];
+
+export default projects;
