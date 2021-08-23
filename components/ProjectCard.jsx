a4c4b9b70978d2c533070f4221bd95fc471c88c9
AleReyLeyva <65025739+AleReyLeyva@users.noreply.github.com>
2021-08-23 17:16:42 +0200

    Update project card styles

--- components/ProjectCard.jsx
+++ components/ProjectCard.jsx
@@ -4,7 +4,7 @@ import CodeSVG from '../SVG/CodeSVG';
 
 export default function ProjectCard({ name, web, code, techs }) {
   const styles = `
-    h-96 lg:h-80 sm:w-full border-2 border-black dark:border-gray-100
+    h-96 lg:h-80 sm:h-72 sm:w-full border-2 border-black dark:border-gray-100
     flex flex-col items-center justify-around
     p-6 rounded-lg 
     text-gray-800 dark:text-white font-medium
@@ -12,15 +12,19 @@ export default function ProjectCard({ name, web, code, techs }) {
 
   return (
     <div className={styles}>
-      <h2 className="text-3xl lg:text-2xl font-semibold">{name}</h2>
-      <h3 className="font-mono font-normal sm:text-lg text-center mb-2">
-        {techs}
-      </h3>
-      <Button href={code}>
-        <CodeSVG />
-        Code
-      </Button>
-      <Button href={web}>🔗 Web</Button>
+      <div>
+        <h2 className="text-3xl lg:text-2xl font-semibold mb-5">{name}</h2>
+        <h3 className="font-mono font-normal sm:text-lg text-center">
+          {techs}
+        </h3>
+      </div>
+      <div>
+        <Button href={code}>
+          <CodeSVG />
+          Code
+        </Button>
+        <Button href={web}>🔗 Web</Button>
+      </div>
     </div>
   );
 }
