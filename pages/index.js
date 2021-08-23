19207248ac48e2c30e95474644b488938e41eade
AleReyLeyva <65025739+AleReyLeyva@users.noreply.github.com>
2021-08-23 15:09:58 +0200

    Presentation title change

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
     <>
       <div className="flex lg:flex-col items-center justify-evenly lg:justify-between h-full mt-10 lg:mt-0">
         <div className="w-2/5 lg:w-full lg:mb-6">
-          <h1 className="sm:text-5xl">Alejandro Rey Leyva</h1>
+          <h1 className="sm:text-5xl">Hi, I am Alejandro Rey Leyva</h1>
           <h2>Junior Full-Stack Web Developer</h2>
         </div>
         <div className="relative w-96 h-96 sm:w-72 sm:h-72 md:h-72 md:w-72">
@@ -22,7 +22,7 @@ export default function Home() {
           />
         </div>
       </div>
-      <div className="w-full flex sm:flex-col justify-center items-center lg:pt-10 pt-20 pb-12">
+      <div className="w-full flex sm:flex-col justify-center items-center mt-20 lg:mt-12">
         <Button href={socialLinks.CV}>
           <DownloadSVG />
           CV
