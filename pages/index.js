0e394a3279885e305a87156880ed62f533a9b2ac
AleReyLeyva <65025739+AleReyLeyva@users.noreply.github.com>
2021-08-23 19:37:18 +0200

    Index title increase line-height

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,9 @@ export default function Home() {
     <>
       <div className="flex lg:flex-col items-center justify-evenly lg:justify-between h-full mt-10 lg:mt-0">
         <div className="w-2/5 lg:w-full lg:mb-6">
-          <h1 className="sm:text-5xl">Hi, I am Alejandro Rey Leyva</h1>
+          <h1 className="sm:text-5xl leading-tight sm:leading-tight">
+            Hi, I am Alejandro Rey Leyva
+          </h1>
           <h2>Junior Full-Stack Web Developer</h2>
         </div>
         <div className="relative w-96 h-96 sm:w-72 sm:h-72 md:h-72 md:w-72">
