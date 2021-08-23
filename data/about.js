e573ae3a5579bc36b1e734c5aabc7d24e6a3041a
AleReyLeyva <65025739+AleReyLeyva@users.noreply.github.com>
2021-08-23 19:36:39 +0200

    About initial set up

---
+++ data/about.js
@@ -0,0 +1,7 @@
+const about = [
+  'Hi, my name is Alejandro Rey Leyva 👋🏼',
+  "I'm from Málaga, Spain 🇪🇸",
+  "I'm full stack web developer 🧑🏻‍💻, especialized in MERN Stack",
+];
+
+export default about;
--- pages/about.js
+++ pages/about.js
@@ -1,5 +1,21 @@
 import React from 'react';
+import about from '../data/about';
 
 export default function About() {
-  return <h1>About Me</h1>;
+  const paragraphStyle = `
+    text-3xl md:text-2xl sm:text-xl my-5 text-center
+  `;
+
+  return (
+    <div>
+      <h1>About Me</h1>
+      <ul>
+        {about.map((paragraph) => (
+          <li className={paragraphStyle} key={Math.random()}>
+            {paragraph}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
