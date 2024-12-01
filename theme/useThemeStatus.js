572fa88b0b232c7eb8096fe940ab24d1aadaaaa2
Alejandro Rey Leyva <alereyleyva@gmail.com>
2024-12-01 10:54:27 +0100

    Remove previous files for blog migration

--- theme/useThemeStatus.js
+++
@@ -1,6 +0,0 @@
-import { useContext } from 'react';
-import { themeStatusContext } from './ThemeStatusProvider';
-
-const useThemeStatus = () => useContext(themeStatusContext);
-
-export default useThemeStatus;
